33ad253774cf6fc9676240f7deba2f51abcd2757
VihanPamudya <vihanpamudya12@gmail.com>
2023-01-05 02:13:15 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,13 @@
+export class Employee {
+    id?: number = 0;
+    firstname: string = '';
+    lastname: string = '';
+    email: string = '';
+    phoneNo: string = '';
+    birthdate: string = '';
+    gender: string = '';
+    education: string = '';
+    jobExperience: number = 0;
+    salary: number = 0;
+    profile: string = '';
+  }
