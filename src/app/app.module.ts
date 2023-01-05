aba2535fa04c2ff659c6526a8bf0636f3f5edf9b
VihanPamudya <vihanpamudya12@gmail.com>
2023-01-05 11:23:35 +0530

    updated changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { EmployeeComponent } from './components/employee/employee.component';
+import { EmployeeComponent } from './components/employees/employee/employee.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { EmployeeAddComponent } from './components/employees/employee-add/employee-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
